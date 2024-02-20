12d57a307e1d33329add6507f668992fa3d6266d
Pavel Kadaňka <121254282+papik13@users.noreply.github.com>
2024-02-20 12:51:18 +0000

    vytvořena struktura stránky

---
+++ Frontend/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>SWA-GAME</h1>
+  </div>
+</template>
+<script setup lang="js">
+  useHead({
+    title: 'Game',
+    meta: [
+      { name: 'description', content: 'My amazing site. With amazing game.' }
+    ],
+    script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+  })
+</script>
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # swaGame
 
+Toto je úkol do SWA
+Cílem projektu je vytvořit jednoduchou hru
+
+Níže jsou použity tyto technologie:
+    - HTML
+    - CSS
+    - PHP
+    - JavaScript
+    - Nuxt 3
